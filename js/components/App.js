e28ed0ef5e34506d4ee99a5284358ab644c13e73
Stefano Verna <s.verna@cantierecreativo.net>
2016-03-25 23:10:21 +0100

    Upgrade packages

--- js/components/App.js
+++ js/components/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
 
-class App extends React.Component {
-  render() {
-    return <div>Hello there!</div>;
-  }
-}
-
-export default App;
+export default () => (
+  <div>Hello there!</div>
+);
